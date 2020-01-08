c8daf28e1252ec816bf1129cc4c252fe6661e057
Dimitar Dimitrov <mitkofr@yahoo.fr>
2020-01-08 13:36:08 +0000

    Use more visible blue

--- printf.sh
+++ printf.sh
@@ -1,27 +1,24 @@
 #! /usr/bin/env bash
 
 Green="$(tput setaf 2 || tput AF 2)"
- Blue="$(tput setaf 4 || tput AF 4)"
+ Blue="$(tput setaf 69 || tput AF 69)"
 Reset="$(tput sgr0 || tput me)"
-
-# %q - print the associated argument shell-quoted, reusable as input
-# %d - print the associated argument as signed decimal number
-# %s - interprets the associated argument literally as string
 
 figlet -f smslant printf
 echo
 
+# text used in the examples
 arr=('In addition to the standard')
 arr+=('format specifications')
 arr+=('described in printf(1)')
 arr+=('and printf(3)')
 
+# examples
 printf "$Green# Min width 35 right align, pad with spaces$Reset\n"
 printf "${Blue}printf '%%35s\\\n' \"\${arr[@]}\"$Reset\n"
 printf '%35s\n' "${arr[@]}"
 echo
 
-# Note: no width (%s), results in left alignment
 printf "$Green# Min width 35 left align, left align (no width means left aligned)$Reset\n"
 printf "${Blue}printf '%%-35s%%s\\\n' \"\${arr[@]}\"$Reset\n"
 printf '%-35s%s\n' "${arr[@]}"
@@ -39,4 +36,11 @@ echo
 
 printf "$Green# * refers to 31$Reset\n"
 printf "${Blue}printf '%%*s\\\n' 31 'Asus z87-pro'$Reset\n"
-printf '%*s\n' 31 'Asus z87-pro'
+printf '%*s\n\n' 31 'Asus z87-pro'
+
+# formats
+cat << 'FORMATS'
+%q - print the associated argument shell-quoted, reusable as input
+%d - print the associated argument as signed decimal number
+%s - interprets the associated argument literally as string
+FORMATS
