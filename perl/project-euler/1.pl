fab63a64af775c3a90e4f281f513fe9d122cfeed
Dimitar Dimitrov <mitkofr@yahoo.fr>
2020-03-22 10:03:30 +0000

    1

---
+++ perl/project-euler/1.pl
@@ -0,0 +1,17 @@
+#! /usr/bin/env perl
+
+use strict;
+use warnings;
+use feature 'say';
+
+my $sum = 0;
+
+for my $i (1..999)
+{
+   unless ($i % 3 and $i % 5)
+   {
+      $sum += $i;
+   }
+}
+
+say $sum;
